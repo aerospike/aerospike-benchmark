b30a7484bfd20a18eea920645569bff194589b3b
ClaytonKnittel <cknittel@wustl.edu>
2020-12-13 15:34:33 -0600

    added copying/license/readme

--- src/test/hdr_histogram_test.c
+++ src/test/hdr_histogram_test.c
@@ -6,7 +6,7 @@
 #include <hdr_histogram/hdr_histogram.h>
 
 
-#define TEST_SUITE_NAME "histogram"
+#define TEST_SUITE_NAME "HDR Histogram"
 
 
 static struct hdr_histogram * h;
@@ -22,13 +22,52 @@ START_TEST(initialization_test)
 }
 END_TEST
 
+
+/**
+ * Any setup code to run each test goes here
+ *
+ * @return  void  
+ */
+static void 
+simple_setup(void) 
+{
+	hdr_init(1, 1000000, 3, &h);
+}
+
+/**
+ * setup's friend. Does the opposite and cleans up the test
+ *
+ * @return  void  
+ */
+static void
+simple_teardown(void)
+{
+	hdr_close(&hist);
+}
+
+
+
+
+
+
+/**
+ * Tests that a simple initialization works
+ */
+START_TEST(initialization_test)
+{
+	ck_assert_msg(hdr_init(1, 1000000, 3, &h) == 0,
+			"HDR Histogram basic initialization failed");
+}
+END_TEST
+
+
 Suite*
 hdr_histogram_suite(void)
 {
 	Suite* s;
 	TCase* tc_core;
 
-	s = suite_create("HDR Histogram");
+	s = suite_create(TEST_SUITE_NAME);
 
 	/* Core test cases */
 	tc_core = tcase_create("Core");
