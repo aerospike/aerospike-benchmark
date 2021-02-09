d54146bc92d2cd4265375e6a6d211cee8dba77f9
ClaytonKnittel <cknittel@wustl.edu>
2021-02-08 18:39:08 -0600

    undo that

--- src/main/main.c
+++ src/main/main.c
@@ -887,7 +887,6 @@ set_args(int argc, char * const * argv, arguments* args)
 					tmp = next_comma + 1;
 				} while (prior != '\0');
 				free(_tmp);
-				args->latency_histogram = true;
 				break;
 			}
 
