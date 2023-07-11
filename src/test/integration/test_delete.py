f41e7d1c4c66803899a9f3605030accac633d7a2
dylan <dwelch@aerospike.com>
2023-07-10 18:32:04 -0700

    remove debug breakpoint

--- src/test/integration/test_delete.py
+++ src/test/integration/test_delete.py
@@ -56,7 +56,6 @@ def test_linear_delete_async_batch():
 def test_linear_delete_subset_batch():
 	# first fill up the database
 	lib.run_benchmark(["--workload", "I", "--start-key", "0", "--keys", "1000", "--batch-size", "5", "--async"])
-	breakpoint()
 	lib.check_for_range(0, 1000)
 	# then delete a subset of the database
 	lib.run_benchmark(["--workload", "DB", "--start-key", "300", "--keys", "500", "--batch-delete-size", "5", "--threads", "2"], do_reset=False)
